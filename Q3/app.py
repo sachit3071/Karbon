c162c499d45bdd75477e9b7f7e80ff8547b400d0
Sachit Gupta <sachit3071@gmail.com>
2024-09-19 21:20:56 +0530

    initial commit

---
+++ Q3/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from textblob import TextBlob
+import requests
+
+import json
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET"])
+def main():
+    return render_template("index.html")
+
+@app.route("/", methods=["POST"])
+def submit_review():
+    print("ehhlasd")
+    if request.is_json:
+        # JSON payload
+        review_text = request.get_json()
+    elif 'file' in request.files:
+        # file upload
+        file = request.files['file']
+        review_text = file.read().decode('utf-8')
+    else:
+        return jsonify({'error': 'No review text provided'}, status = 400)
+    sentiment = []
+    # print(review_text)
+    for review in review_text:
+        blob = TextBlob(review.review_text)
+        rev = {'origianl_review_text': review.review_text,
+               'sentiment_category': blob.sentiment.polarity}
+    return jsonify(sentiment, status=200, mimetype='application/json')
+
+if __name__ == "__main__":
+    app.run(debug=False, port = 5000)
